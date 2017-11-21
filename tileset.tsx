9ce84b11629fb5cd9f0de3c859d190350aed614f
j2y <iop01447@naver.com>
2017-11-21 10:53:12 +0900

    커밋하다가 다 날라감 진짜 짜증난다

---
+++ tileset.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="130" tileheight="130" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="130" height="130" source="../DinoMelt/Graphics/tile/down1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="130" height="130" source="../DinoMelt/Graphics/tile/down2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="130" height="130" source="../DinoMelt/Graphics/tile/down3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="130" height="130" source="../DinoMelt/Graphics/tile/in1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="130" height="130" source="../DinoMelt/Graphics/tile/in2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="130" height="130" source="../DinoMelt/Graphics/tile/up1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="130" height="130" source="../DinoMelt/Graphics/tile/up2.png"/>
+ </tile>
+ <tile id="7">
+  <image width="130" height="130" source="../DinoMelt/Graphics/tile/up3.png"/>
+ </tile>
+ <tile id="8">
+  <image width="130" height="130" source="../DinoMelt/Graphics/tile/up4.png"/>
+ </tile>
+ <tile id="9">
+  <image width="130" height="130" source="../DinoMelt/Graphics/tile/up5.png"/>
+ </tile>
+</tileset>
